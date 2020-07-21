e3908f7fd47f48c0e9cae209ab535bdcad50289c
DanielBrmdz <47276519+DanielBrmdz@users.noreply.github.com>
2020-07-20 20:28:38 -0500

    ejemplo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('HOLA MUNDO');
+  res.send('NUEVO HOLA MUNDO...');
 });
 
 app.listen(3000, function () {
